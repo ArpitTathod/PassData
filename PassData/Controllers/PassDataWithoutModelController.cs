a02df8c65ae3876b23bd468c7e26526eb05dabfb
Arpit <arpit16tathod@gmail.com>
2023-10-18 12:33:55 +0530

    Add project files.

---
+++ PassData/Controllers/PassDataWithoutModelController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PassData.Controllers
+{
+    public class PassDataWithoutModelController : Controller
+    {
+        // GET: PassDataWithoutModel
+        public ActionResult Index()
+        {
+            ViewBag.age = 20;
+            ViewBag.fullname  = "Arpit tathod";
+            ViewBag.Status = true;
+            ViewBag.Price = 4.5;
+            ViewBag.Brithday =  DateTime.Now;
+            ViewBag.Gender = "male";
+            return View();
+        }
+    }
+}
---
+++ PassData/Controllers/PassObjectfromControllertoViewController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using PassData.Models;
+
+namespace PassData.Controllers
+{
+    public class PassObjectfromControllertoViewController : Controller
+    {
+        // GET: PassObjectfromControllertoView
+        public ActionResult Index()
+        {
+            PassObjectProduct  product =new PassObjectProduct()
+            {
+                Id = "p01",
+                Name = "Name 1",
+                Photo ="133395009803041258.jpg",
+                Price = "5.5",
+                Quantity = "4",
+
+
+
+            };
+            ViewBag.Product = product; 
+            return View();
+        }
+    }
+}
